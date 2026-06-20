e2699c3a086f50ad4f0290fcfead951a350b6375
SadKat-builds <rohit.suthar.1935@gmail.com>
2026-06-21 02:30:55 +0530

    Added Repl and Command parsing

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ enum Command {
 }
 
 fn main() {
+    let mut start_store = Store::new();
     loop {
         print!("Welcome to Poseidon! ");
-        io::stdout().flush().unwrap_err();
+        io::stdout().flush().unwrap();
 
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
@@ -24,6 +25,57 @@ fn main() {
 
         if input.trim() == "exit" {
             break;
+        }
+
+        match command_name {
+            Some("Get") => {
+                 if let Some(k) = key {
+                    let _cmd = Command::Get { key: k.to_string() };
+                    match _cmd {
+                        Command::Get { key } => {
+                            println!("Executing Get Operation For : {}", key);
+                            match start_store.get(key) {
+                                Some(value) => println!("Value : {}", value),
+                                None => println!("Key not found"),
+                            }
+                        }
+                        _ => {}
+                    }
+                 } else {
+                    println!("You must provide a key value");
+                 }
+            }
+            Some("Put") => {
+                if let (Some(k),Some(v)) = (key,value) {
+                    let _cmd = Command::Put{key:k.to_string(), value:v.to_string()};
+                    match _cmd {
+                        Command::Put {key , value} => {
+                            println!("Executing Put Operation For : {} , Value : {}", key , value);
+                            start_store.put(key, value);
+                        }
+                        _ => {}
+                    }
+                } else {
+                    println!("You must provide a key value and a value");
+                }
+            }
+            Some("Delete") => {
+                if let Some(k) = key {
+                    let _cmd = Command::Delete{key:k.to_string()};
+                    match _cmd {
+                        Command::Delete {key} => {
+                            println!("Executing Delete Operation For : {}", key);
+                            start_store.delete(key);
+                        }
+                        _ => {}
+                    }
+                } else {
+                    println!("You must provide a key value");
+                }
+            }
+            _=>{
+                println!("Unknown Command");
+            }
         }
     }
 }
--- src/store/memory.rs
+++ src/store/memory.rs
@@ -12,6 +12,7 @@ impl Store {
         self.map.get(&key)  
     }
     pub fn put(&mut self, key: String, value: String) {
+           if self.map.contains_key(&key) {println!("The key already exists in the database");}
            self.map.insert(key,value);
     }
     pub fn delete(&mut self, key : String) {
